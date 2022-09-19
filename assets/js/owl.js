b7779f0958ca3bd2f3f28e155a82284373be1019
ibrahimIJ-1 <ibrahimjindybuisness@gmail.com>
2022-09-19 12:12:02 +0300

    ad

--- assets/js/owl.js
+++ assets/js/owl.js
@@ -1,77 +1,77 @@
 $(".categories_carousel").owlCarousel({
-    loop: true,
-    margin: 5,
-    responsiveClass: true,
-    rtl: true,
-    responsive: {
-        0: {
-            items: 3,
-            nav: false,
-            dots: false,
-            loop: false,
-        },
-        300: {
-            items: 3,
-            nav: false,
-            dots: false,
-            loop: false,
-        },
-        600: {
-            items: 4,
-            nav: false,
-            dots: false,
-            loop: false,
-        },
-        800: {
-            items: 5,
-            nav: false,
-            dots: false,
-            loop: false,
-        },
-        1000: {
-            items: 8,
-            nav: false,
-            loop: false,
-            dots: false,
-        },
+  loop: true,
+  margin: 5,
+  responsiveClass: true,
+  rtl: true,
+  responsive: {
+    0: {
+      items: 3,
+      nav: false,
+      dots: false,
+      loop: false,
     },
+    300: {
+      items: 3,
+      nav: false,
+      dots: false,
+      loop: false,
+    },
+    600: {
+      items: 4,
+      nav: false,
+      dots: false,
+      loop: false,
+    },
+    800: {
+      items: 5,
+      nav: false,
+      dots: false,
+      loop: false,
+    },
+    1000: {
+      items: 8,
+      nav: false,
+      loop: false,
+      dots: false,
+    },
+  },
 });
 
 $("#images").owlCarousel({
-    loop: true,
-    margin: 5,
-    responsiveClass: true,
-    rtl: true,
-    responsive: {
-        0: {
-            items: 3,
-            nav: false,
-            dots: true,
-            loop: true,
-        },
-        300: {
-            items: 3,
-            nav: false,
-            dots: true,
-            loop: true,
-        },
-        600: {
-            items: 4,
-            nav: false,
-            dots: true,
-            loop: true,
-        },
-        800: {
-            items: 5,
-            nav: false,
-            dots: true,
-            loop: true,
-        },
-        1000: {
-            items: 8,
-            nav: false,
-            loop: true,
-            dots: true,
-        },
+  loop: true,
+  margin: 50,
+  responsiveClass: true,
+  rtl: true,
+  responsive: {
+    0: {
+      items: 3,
+      nav: false,
+      dots: true,
+      loop: true,
     },
+    300: {
+      items: 3,
+      nav: false,
+      dots: true,
+      loop: true,
+    },
+    600: {
+      items: 4,
+      nav: false,
+      dots: true,
+      loop: true,
+    },
+    800: {
+      items: 5,
+      nav: false,
+      dots: true,
+      loop: true,
+    },
+    1000: {
+      items: 8,
+      nav: false,
+      loop: true,
+      dots: true,
+    },
+  },
 });
